18ae8b123a853aa0fb63788b913752b1ffedbb9c
pravinya-07 <pravinya.k.in@rupeek.com>
2023-09-10 20:30:05 +0530

    Added frontend and backend

---
+++ backend/demo 2/src/main/java/com/ui/demo/repo/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.ui.demo.repo;
+
+import com.ui.demo.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends MongoRepository<Student,String> {
+}
---
+++ frontend/basic_ui/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Student from './components/student';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Student/>
+    </div>
+  );
+}
+
+export default App;
